fdadba1aedf2b23118e105fb40eb4213ed527a1b
Kseniya <miron2014857@gmail.com>
2024-11-28 12:57:23 +0300

    add files

--- src/main/java/org/example/tms_web_calculator_servlets/CalcFilters.java
+++ src/main/java/org/example/tms_web_calculator_servlets/CalcFilters.java
@@ -1,4 +1,31 @@
 package org.example.tms_web_calculator_servlets;
 
-public class CalcFilters {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/calc")
+public class CalcFilters extends HttpFilter {
+@Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    String expression = request.getParameter("expression");
+    Calculation calc = new Calculation();
+
+    if(expression == null||expression.isEmpty()) {
+        response.getWriter().println("No expression provided");
+        return;
+    }
+
+    if(calc.getOperator(expression).equals("")) {
+        response.getWriter().println("Invalid operator.Please enter a valid operator");
+        return;
+    }
+
+    chain.doFilter(request, response);
+}
 }
--- src/main/java/org/example/tms_web_calculator_servlets/Calculation.java
+++ src/main/java/org/example/tms_web_calculator_servlets/Calculation.java
@@ -1,4 +1,46 @@
 package org.example.tms_web_calculator_servlets;
 
-public class Calculation {
-}
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+    public class Calculation {
+        private String expr;
+
+        public String getOperator(String expr){
+
+            String operator = "";
+            if (expr.contains("+")) {
+                operator = "+";
+            } else if (expr.contains("-")) {
+                operator = "-";
+            } else if (expr.contains("*")) {
+                operator = "*";
+            } else if (expr.contains("/")) {
+                operator = "/";
+            }
+
+            return operator;
+        }
+
+        public String getCalc(String expr){
+            String result;
+            String operator = getOperator(expr);
+            String[] parts = expr.split(Pattern.quote(String.valueOf(operator)));
+
+            if(Objects.equals(operator, "+"))
+                result = String.valueOf(Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]));
+            else if(Objects.equals(operator, "-"))
+                result = String.valueOf(Integer.parseInt(parts[0]) - Integer.parseInt(parts[1]));
+            else if(Objects.equals(operator, "*"))
+                result = String.valueOf(Integer.parseInt(parts[0]) * Integer.parseInt(parts[1]));
+            else if(Objects.equals(operator, "/")) {
+                if(parts[1].equals("0"))
+                    return "Division by zero";
+                result = String.valueOf(Double.parseDouble(parts[0]) / Double.parseDouble(parts[1]));
+            }
+            else
+                result = "Error.Incorrect input...";
+
+            return result;
+        }
+    }
